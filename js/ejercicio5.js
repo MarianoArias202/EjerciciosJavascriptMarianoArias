636b20fca68a775a945e4a95670c5c97de0675d4
mariano202 <marianoaarias202@gmail.com>
2023-10-23 17:08:09 -0300

    ejercicio6

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -3,6 +3,6 @@ pida otro número y escriba el resultado de sumar estos dos números.
 Nota: Tener en cuenta la siguiente función: parseInt
 https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt */
 
-let numero1= parseInt(prompt("Ingrese un numero"));
-let numero2= parseInt(prompt("Ingrese un numero"));
-document.write("<h1>El resultado de la suma es: " + (numero1 + numero2) + "</h1>");
+// let numero1= parseInt(prompt("Ingrese un numero"));
+// let numero2= parseInt(prompt("Ingrese un numero"));
+// document.write("<h1>El resultado de la suma es: " + (numero1 + numero2) + "</h1>");
