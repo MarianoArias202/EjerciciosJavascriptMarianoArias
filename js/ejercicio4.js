a10d45ce9119937192ecbbe7b2459f91f2ba1a9a
mariano202 <marianoaarias202@gmail.com>
2023-10-30 20:04:29 -0300

    preparacion para el tp2

---
+++ js/ejercicio4.js
@@ -0,0 +1,9 @@
+/* 4.- Escribe un programa de dos líneas que pida el nombre del usuario 
+con un prompt y escriba un texto que diga «Hola nombreUsuario»
+Ejemplo: 
+
+input: Coder 
+Output: Hola Coder */
+
+ let nombreusuario = prompt("Ingrese su nombre de usuario");
+document.write("<h1>Hola " + nombreusuario + "</h1>"); 
